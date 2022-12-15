91e5d7100f1d0db99e873ac3593bb854d04ebae9
Vladyslav Geyna <ipz215_gvs@student.ztu.edu.ua>
2022-12-15 16:38:35 +0200

    Included bootstrap

--- themes/light/layout.php
+++ themes/light/layout.php
@@ -1,3 +1,9 @@
+<?php
+/** @var string $title */
+/** @var string $content */
+/** @var string $siteName */
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,9 +11,20 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title><?= $siteName ?> — <?= $title ?></title>
+    <link rel="stylesheet" href="/themes/light/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/themes/light/css/style.css">
+
+
 </head>
 <body>
+
+
     <?=$content?>
+
+
+
+
+    <script src="/themes/light/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
