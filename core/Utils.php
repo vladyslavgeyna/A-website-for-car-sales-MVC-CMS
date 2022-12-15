f274765a4c990ef9f066255dfb38a6397452e04a
Vladyslav Geyna <ipz215_gvs@student.ztu.edu.ua>
2022-12-15 12:54:03 +0200

    Created Utils class

---
+++ core/Utils.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace core;
+
+class Utils
+{
+    public static function filterArray($array, $fieldsList): array
+    {
+        $filteredArray = [];
+        foreach ($array as $key => $value)
+        {
+            if (in_array($key, $fieldsList))
+            {
+                $filteredArray[$key] = $value;
+            }
+        }
+        return $filteredArray;
+    }
+}
